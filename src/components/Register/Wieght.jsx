dbbc9229a738e34a8799766fcb40ee08f27172c6
unknown <honeytose@gmail.com>
2023-10-07 21:12:14 +0700

    fix and increse btn function

--- src/components/Register/Wieght.jsx
+++ src/components/Register/Wieght.jsx
@@ -1,29 +1,25 @@
 import React from 'react'
 
-const Wieght = ({setWeight,weight}) => {
+const Wieght = ({ setWeight, weight }) => {
   return (
     <div>
-        <div className='hidden'>
-        <div className='relative top-20 ' >
+        <div className='pt-20' >
           <h2 className='text-3xl text-white text-center'>What’s your weight?</h2>
           <p className='text-center text-zinc-500'>You can always change this later</p>
         </div>
 
-        <div className='relative top-40 flex justify-center  align-middle items-end'>
+        <div className='mt-40 flex justify-center  align-middle items-end'>
           <p className='text-center text-8xl text-white '> {weight} </p>
           <p className='text-xl  text-white pb-4'>kg</p>
         </div>
 
-        <div className='relative top-48 px-5'>
-          <input type="range" name="" id="" onChange={(e)=>setWeight(e.target.value)} value={weight} min="0" max="100" step="0.1" className='w-full range_bar_weigth' />
+        <div className='px-5'>
+          <input type="range" name="" id="" onChange={(e) => setWeight(e.target.value)} value={weight} min="0" max="100" step="0.1" className='w-full range_bar_weigth' />
           <div className='scale_weight'>
-            
+
           </div>
         </div>
-        <div className=' relative bg-zinc-800  rounded-md top-96 mx-5 bg-btn'>
-          <button className='text-2xl font-bold text-white text-center  bg-zinc-800 relative  btn'>Next</button>
-        </div>
-      </div>
+      
     </div>
   )
 }
